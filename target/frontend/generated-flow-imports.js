2a3b6a2f26bd132767c20e88ae3c8e124b574ceb
Panida Phak-insee <panida.1073@gmail.com>
2019-09-02 10:32:08 +0700

    third-commit

---
+++ target/frontend/generated-flow-imports.js
@@ -0,0 +1,20 @@
+const div = document.createElement('div');
+div.innerHTML = '<custom-style><style include="lumo-color lumo-typography"></style></custom-style>';
+document.head.insertBefore(div.firstElementChild, document.head.firstChild);
+
+import '@vaadin/vaadin-lumo-styles/icons.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-ordered-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-button/theme/lumo/vaadin-button.js';
+import '@vaadin/flow-frontend/flow-component-renderer.js';
+import '@vaadin/vaadin-form-layout/theme/lumo/vaadin-form-item.js';
+import '@vaadin/vaadin-text-field/theme/lumo/vaadin-text-field.js';
+import '@vaadin/vaadin-notification/theme/lumo/vaadin-notification.js';
+import '@vaadin/vaadin-form-layout/theme/lumo/vaadin-form-layout.js';
+import '@vaadin/vaadin-checkbox/theme/lumo/vaadin-checkbox.js';
+import '@vaadin/vaadin-date-picker/theme/lumo/vaadin-date-picker.js';
+import '@vaadin/flow-frontend/datepickerConnector.js';
